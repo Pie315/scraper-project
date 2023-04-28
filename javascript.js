dd090dbf6673e48047bf3d23b5baae9d5ed27b69
Pie315 <devonkaelberer@gmail.com>
2023-04-28 13:31:37 -0400

    basic working order

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
-async function logJSON() {
-    const response = await fetch
+async function fetchAsync(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
 }
+
+function clicked() {
+    const output = document.querySelector(".output")
+    console.log("clicked")
+    output.textContent = fetchAsync("https://www.google.com");
+}
+
+const button = document.querySelector(".run");
+button.addEventListener("click", clicked());
+
